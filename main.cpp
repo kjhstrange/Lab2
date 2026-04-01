0d83930b498b98d1349cfeb5e02c22ee601132d0
kjhstrange <kjhstrange@gmail.com>
2026-04-01 17:30:45 +0900

    test

--- main.cpp
+++ main.cpp
@@ -7,13 +7,19 @@ int N,M;
 vector<int> vec;
 
 void func(int cnt) {
+   int d = 1;
+   while(d <= N || d <= M){
+      if(N % d == 0 && M % d == 0){
+         cnt++;
+         vec.push_back(d);
+      }
+      d++;
+   }
 
-
-
-
-
-
-
+   cout << cnt << endl;
+   for(int i = 0;i < vec.size();i++){
+      cout << vec[i] << " ";
+   }
 }
 
 int main() {
