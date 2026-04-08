e147a0e5af768eb9bc37010c0cb80b6878637786
kjhstrange <kjhstrange@gmail.com>
2026-04-08 10:45:42 +0900

    modify func

--- main.cpp
+++ main.cpp
@@ -7,22 +7,26 @@ int N,M;
 vector<int> vec;
 
 void func(int cnt) {
-   int d = 1;
-   while(d <= N || d <= M){
-      if(N % d == 0 && M % d == 0){
-         cnt++;
-         vec.push_back(d);
+   if(vec[cnt] >= pow(10, M-1)){
+      for(int i=cnt;i<vec.size();i++){
+         cout << vec[i] << endl;
       }
-      d++;
+      return;
    }
 
-   cout << cnt << endl;
-   for(int i = 0;i < vec.size();i++){
-      cout << vec[i] << " ";
+   int cnt_next = vec.size();
+   for(int i=cnt;i<cnt_next;i++){
+      for(int j=vec[i] % 10 + 1;j<=N;j++){
+         vec.push_back(vec[i]*10+j);
+      }
    }
+   func(cnt_next);
 }
 
 int main() {
    cin >> N >> M;
+   for(int i=0;i<N;i++){
+      vec.push_back(i+1);
+   }
    func(0);
 }
